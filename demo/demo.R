b332b70328e98122b3a36a00b55a3ed5c3dac73a
Nicolas Payette <nicolaspayette@gmail.com>
2024-04-25 19:38:17 +0100

    update POSEIDON jar and demo file

--- demo/demo.R
+++ demo/demo.R
@@ -2,7 +2,7 @@ options(java.parameters = "-Xmx8g")
 devtools::load_all()
 
 input_folder <-
-  fs::path_home("workspace", "epo_inputs")
+  fs::path_home("workspace", "POSEIDON", "epo", "epo_inputs")
 
 scenario_path <-
   fs::path(input_folder, "tests", "scenarios", "EpoPathPlannerAbundanceScenario.yaml")
@@ -16,38 +16,28 @@ scenario |>
 scenario |>
   set_parameter_value("inputFolder.path", input_folder)
 
-scenario |>
-  get_parameter_value("purseSeinerFleetFactory.destinationStrategyFactory.fadModuleFactory.dampen")
-
-scenario |>
-  set_parameter_value("purseSeinerFleetFactory.destinationStrategyFactory.fadModuleFactory.dampen", 0.75)
-
-scenario |>
-  get_parameter_value("purseSeinerFleetFactory.destinationStrategyFactory.fadModuleFactory.dampen")
-
 sim <-
   scenario |>
   new_simulation()
 
-sim |> step(36)
+sim |> step(360)
 
 get_dataset_names(sim)
 
-daily_time_series <-
-  sim |>
-  get_dataset("Daily time series")
-
-daily_time_series |>
-  get_table_names() |>
-  purrr::chuck(1) |>
-  purrr::map(\(table) daily_time_series |> get_table(table))
-
+# daily_time_series <-
+#   sim |>
+#   get_dataset("Daily time series")
+#
+# daily_time_series |>
+#   get_table_names() |>
+#   purrr::map(\(table) daily_time_series |> get_table(table))
+#
 actions <-
   sim |>
   get_dataset("Purse-seiner events") |>
   get_table("Actions")
 
-subtrips <-
+trips <-
   sim |>
   get_dataset("Purse-seiner events") |>
   get_table("Trips")
